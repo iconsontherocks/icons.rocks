059fa89408250388f0981e1178cddde800e6fc6a
Pablo Salgado <pabloalexandre.nave@gmail.com>
2020-10-30 02:39:31 -0300

    refactor: remove useless useEffect

--- modules/home/home.tsx
+++ modules/home/home.tsx
@@ -1,14 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 import { IconList } from './components/icon-list';
 import { getIcons, IconsMenu } from '../icons';
 
 export const Home = () => {
-  const [icons, setIcons] = useState([]);
-
-  useEffect(() => {
-    setIcons(getIcons());
-  }, []);
+  const [icons] = useState(getIcons());
 
   return (
     <>
